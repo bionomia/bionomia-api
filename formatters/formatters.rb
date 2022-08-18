57db369a5c1c651ac6f3903bb41e834e9daf40c1
David Shorthouse <davidpshorthouse@gmail.com>
2022-08-17 23:22:48 -0400

    Fix bug in previous

--- formatters/formatters.rb
+++ formatters/formatters.rb
@@ -103,7 +103,7 @@ module Sinatra
           name: n[:_source][:fullname],
           givenName: n[:_source][:given],
           familyName: n[:_source][:family],
-          alternateName: [n[:_source][:fullname_reverse]] + n[:_source][:other_names]
+          alternateName: [n[:_source][:fullname_reverse]] + n[:_source][:other_names],
           co_collector: n[:_source][:co_collectors].map do |colleague|
             - same_as = colleague[:orcid] ? "https://orcid.org/#{colleague[:orcid]}" : "http://www.wikidata.org/entity/#{colleague[:wikidata]}"
             {
